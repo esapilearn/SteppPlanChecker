cc2802c39ff7929e3d55b7ead911436a74ad271e
Robert Vincent Stepp <robertvstepp@gmail.com>
2020-12-16 10:20:52 -0800

    Changed title.

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -4,7 +4,7 @@ namespace SteppPlanChecker.ViewModels
 {
     public class MainViewModel : BindableBase
     {
-        private string _title = "ESAPIX Starter Application";
+        private string _title = "Stepp ESAPIX Starter Application";
         public string Title
         {
             get { return _title; }
